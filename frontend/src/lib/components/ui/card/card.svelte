5ffd077fd70f4f6e2eea7bb278ac6de4e5a32bcb
Fernando Araoz <fernando@araoz.dev>
2026-07-10 10:05:24 -0500

    ui: improve shadcn styles

--- frontend/src/lib/components/ui/card/card.svelte
+++ frontend/src/lib/components/ui/card/card.svelte
@@ -16,7 +16,7 @@
 	data-slot="card"
 	data-size={size}
 	class={cn(
-		'border-border/80 bg-card/85 text-card-foreground gap-(--card-spacing) overflow-hidden rounded-xl border py-(--card-spacing) text-xs/relaxed shadow-sm shadow-foreground/8 backdrop-blur-sm [--card-spacing:--spacing(4)] has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(3)] *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl group/card flex flex-col',
+		'border-border/80 bg-card/85 text-card-foreground gap-(--card-spacing) overflow-hidden rounded-xl border py-(--card-spacing) text-xs/relaxed shadow-sm shadow-foreground/2 backdrop-blur-sm [--card-spacing:--spacing(4)] has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(3)] *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl group/card flex flex-col',
 		className
 	)}
 	{...restProps}
